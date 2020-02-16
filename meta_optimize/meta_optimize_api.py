cf31a865a95c0f1a642e403d5c90bce6b778c7ee
SimonBlanke <simonblanke@hotmail.de>
2020-02-16 17:04:59 +0100

    change api

--- meta_optimize/meta_optimize_api.py
+++ meta_optimize/meta_optimize_api.py
@@ -15,29 +15,32 @@ X, y = np.array([0]), np.array([0])
 
 
 class MetaOptimize:
-    def __init__(self,):
+    def __init__(self, n_iter, max_time, optimizer, n_jobs, init_config):
         self.opt = Hyperactive(X, y, memory="short", verbosity=3)
 
-    def optimize(
-        self,
-        optimizer,
-        hyperactive_config={
-            "n_iter": 100,
-            "max_time": None,
-            "optimizer": "RandomSearch",
-            "n_jobs": 1,
-            "init_config": None,
-        },
-    ):
+        self.n_iter = n_iter
+        self.max_time = max_time
+        self.optimizer = optimizer
+        self.n_jobs = n_jobs
+        self.init_config = init_config
 
-        search_config = meta_opt_search_config[optimizer]
-        self.opt.search(search_config, **hyperactive_config)
+    def optimize(self, to_optimize):
+
+        search_config = meta_opt_search_config[to_optimize]
+        self.opt.search(
+            search_config,
+            n_iter=self.n_iter,
+            max_time=self.max_time,
+            optimizer=self.optimizer,
+            n_jobs=self.n_jobs,
+            init_config=self.init_config,
+        )
 
         model = list(search_config.keys())[0]
         best_para = self.opt.results[model]
         score = self.opt.best_scores[model]
 
-        best_opt_para = {optimizer: best_para}
+        best_opt_para = {to_optimize: best_para}
         # print("\nbest_opt_para =", best_opt_para)
         # print("score         =", score)
         return best_opt_para
